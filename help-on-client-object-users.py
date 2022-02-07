ce742146c571ec7be158ffadd098cfd1f092ce93
Dhanya <60929233+DHANYAV20@users.noreply.github.com>
2022-02-07 23:59:25 +0530

    Add files via upload

---
+++ help-on-client-object-users.py
@@ -0,0 +1,10 @@
+import boto3
+aws_mag_con=boto3.session.Session(profile_name="root")
+#iam,ec2 and s3
+iam_con_cli=aws_mag_con.client(service_name="iam",region_name="us-west-2")
+ec2_con_cli=aws_mag_con.client(service_name="ec2",region_name="us-west-2")
+s3_con_cli=aws_mag_con.client(service_name="s3",region_name="us-west-2")
+
+#List all iam users using client object
+response=iam_con_cli.list_users()
+print(response['Users'])
