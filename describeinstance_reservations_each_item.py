a88baf66974dac25f854c08599d40d417f92155d
Dhanya <60929233+DHANYAV20@users.noreply.github.com>
2022-02-08 09:26:04 +0530

    Initial Commit

---
+++ describeinstance_reservations_each_item.py
@@ -0,0 +1,17 @@
+import boto3
+aws_mag_con=boto3.session.Session(profile_name="root")
+#iam,ec2 and s3
+iam_con_cli=aws_mag_con.client(service_name="iam",region_name="us-west-2")
+ec2_con_cli=aws_mag_con.client(service_name="ec2",region_name="us-west-2")
+s3_con_cli=aws_mag_con.client(service_name="s3",region_name="us-west-2")
+'''
+#List all iam users using client object
+response=iam_con_cli.list_users()
+for each_item in response['Users']:
+    print(each_item['UserName'])
+'''
+# list all ec2 instances ids
+response = ec2_con_cli.describe_instances()
+for each_item in response['Reservations']:
+    print(each_item)
+    print('--------------------------')
